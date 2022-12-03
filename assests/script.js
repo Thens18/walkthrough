a79bbe78fb304aeb16be6b3928bb00189372283b
Thens18 <thensthennavan3@gmail.com>
2022-12-03 10:29:05 +0530

    text animation added

--- assests/script.js
+++ assests/script.js
@@ -6,3 +6,59 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 })
 
+
+
+async function init () {
+    const node = document.querySelector("#type-text")
+    
+    await sleep(1000)
+    node.innerText = ""
+    await node.type('Choose, ')
+    
+    while (true) {
+      await node.type('HackerRank !')
+      await sleep(2000)
+      await node.delete('HackerRank !')
+      await node.type('OverTheWire !')
+      await sleep(2000)
+      await node.delete('OverTheWire !')
+      await node.type('HTB !')
+      await sleep(2000)
+      await node.delete('HTB !')
+      await node.type('CodeChef !')
+      await sleep(2000)
+      await node.delete('CodeChef !')
+    }
+  }
+  
+  
+  // Source code 🚩
+  
+  const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+  
+  class TypeAsync extends HTMLSpanElement {
+    get typeInterval () {
+      const randomMs = 100 * Math.random()
+      return randomMs < 50 ? 10 : randomMs
+    }
+    
+    async type (text) {
+      for (let character of text) {
+        this.innerText += character
+        await sleep(this.typeInterval)
+      }
+    }
+    
+    async delete (text) {
+      for (let character of text) {
+        this.innerText = this.innerText.slice(0, this.innerText.length -1)
+        await sleep(this.typeInterval)
+      }
+    }
+  }
+  
+  customElements.define('type-async', TypeAsync, { extends: 'span' })
+  
+  
+  init()
+  
